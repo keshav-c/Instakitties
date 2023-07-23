e45279ce95add870ff793852f2aff007676b4ada
Keshav Chakravarthy <keshav-c@users.noreply.github.com>
2023-07-24 00:03:22 +0530

    Event handler that logs the kitty name

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,7 +1,11 @@
 function Post({ image, name }) {
+  const handleClick = (catName) => {
+    console.log('You clicked ' + catName);
+  };
+
   return (
     <li className="post-component">
-      <button>
+      <button onClick={() => handleClick(name)}>
         <img src={image} alt={name}></img>
         <p className="post-name">{name}</p>
       </button>
