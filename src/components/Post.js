bfe426f40e6d1e58604571a7bb8c2a23caa4d337
Keshav Chakravarthy <keshav-c@users.noreply.github.com>
2023-07-24 00:07:19 +0530

    hover mouse handlers

--- src/components/Post.js
+++ src/components/Post.js
@@ -3,9 +3,21 @@ function Post({ image, name }) {
     console.log('You clicked ' + catName);
   };
 
+  const handleMouseEnter = (catName) => {
+    console.log(`Hello, ${catName}`);
+  };
+
+  const handleMouseLeave = (catName) => {
+    console.log(`Goodbye, ${catName}`);
+  };
+
   return (
     <li className="post-component">
-      <button onClick={() => handleClick(name)}>
+      <button
+        onClick={() => handleClick(name)}
+        onMouseEnter={() => handleMouseEnter(name)}
+        onMouseLeave={() => handleMouseLeave(name)}
+      >
         <img src={image} alt={name}></img>
         <p className="post-name">{name}</p>
       </button>
